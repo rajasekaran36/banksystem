645b3dddd84a3f9710a7f3e38beae8948782bad8
Rajasekaran Selvaraj <rajasekaran.s@kgisl.com>
2020-02-18 16:30:16 +0530

    v2

--- src/main/java/com/kgisl/basic/App.java
+++ src/main/java/com/kgisl/basic/App.java
@@ -1,13 +1,14 @@
 package com.kgisl.basic;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Customer aCustomer = new Customer("raja","BPUPX000X",123456789123L);
+        BankAccount aBankAccount = new BankAccount(aCustomer, 3633621123456L, 0.0);
+        CurrentAccount ca = new CurrentAccount(aBankAccount, 100000.0, 0.0);
+
+        System.out.println(ca.getAccount().getAccountNumber());
+        System.out.println(ca.getAccount().getCustomer().getCustomerName());
     }
 }
